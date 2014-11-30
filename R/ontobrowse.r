98ff7fb6c364a68088e6de02da67989102ac5108
Loredana Cirstea <loredana.cirstea@gmail.com>
2014-11-30 06:54:33 +0200

    changed to uuids in R, starter onto framework

--- R/ontobrowse.r
+++ R/ontobrowse.r
@@ -2,7 +2,7 @@
 #"https://raw.githubusercontent.com/ctzurcanu/smp/master/data/term.csv"
 #rels
 #"https://raw.githubusercontent.com/ctzurcanu/smp/master/data/term_relation.csv"
-#ontoList
+#onto_list
 #"https://raw.githubusercontent.com/ctzurcanu/smp/master/data/ontologies.csv"
 #smp
 #"https://raw.githubusercontent.com/ctzurcanu/smp/master/data/jos_sliced_api.csv"
@@ -13,10 +13,11 @@ x <- getURL("https://raw.githubusercontent.com/ctzurcanu/smp/master/data/term.cs
 terms <- read.csv(text = x)
 y <- getURL("https://raw.githubusercontent.com/ctzurcanu/smp/master/data/term_relation.csv")
 rels <- read.csv(text = y)
-z <- getURL("https://raw.githubusercontent.com/ctzurcanu/smp/master/data/ontologies.csv")
-ontoList <- read.csv(text = z)
 s <- getURL("https://raw.githubusercontent.com/ctzurcanu/smp/master/data/jos_sliced_api.csv")
 smp <- read.csv(text = s)
+subject_apps <- readRDS("data/subject_apps.rds")
+onto_list <- readRDS("data/onto_list.rds")
+app_list = list()
 
 ancestry <- function(terms, rels, term, lang, origin, returnIds = TRUE) {
   path = term
@@ -25,7 +26,7 @@ ancestry <- function(terms, rels, term, lang, origin, returnIds = TRUE) {
   }
   id = term;
   while((id != origin) == TRUE) {
-    parent = rels[rels$term1 == id & rels$relation == 1,"term2"][1];
+    parent = as.character(rels[rels$uuid1 == id & rels$relation == 1,"uuid2"][1])
     path = c(path, parent);
     id = parent;
   }
@@ -35,21 +36,21 @@ ancestry <- function(terms, rels, term, lang, origin, returnIds = TRUE) {
   else {
     termsNames = c();
     for(id in path) {
-      termsNames = c(termsNames, as.character(terms[terms$term_id == id & terms$lang == lang, "term"]));
+      termsNames = c(termsNames, as.character(terms[terms$uuid == id & terms$lang == lang, "term"]));
     }
     return(termsNames);
   }
 }
 children <- function(terms, rels, term, lang, returnIds = TRUE) {
   termId = term
-  kidsIds = rels[rels$term2 == termId & rels$relation == 1,"term1"];
+  kidsIds = as.character(rels[rels$uuid2 == termId & rels$relation == 1,"uuid1"])
   if(returnIds == TRUE) {
     result <- kidsIds;
   }
   else {
     kids = c();
     for(id in kidsIds) {
-      termN = terms[terms$term_id == id & terms$lang == lang, "term"];
+      termN = as.character(terms[terms$uuid == id & terms$lang == lang, "term"])
       termN <- termN[!is.na(termN)][1];
       kids = c(kids, termN);
     }
@@ -58,8 +59,8 @@ children <- function(terms, rels, term, lang, returnIds = TRUE) {
   return(result)
 }
 siblings <- function(terms, rels, term, lang, returnIds = TRUE) {
-  if(returnIds == TRUE) { sibs <- children(terms, rels, rels[rels$term1 == term, "term2"]);}
-  else { sibs <- children(terms, rels, rels[rels$term1 == term, "term2"], lang, returnIds = FALSE);}
+  if(returnIds == TRUE) { sibs <- children(terms, rels, rels[rels$uuid1 == term, "uuid2"]);}
+  else { sibs <- children(terms, rels, as.character(rels[rels$uuid1 == term, "uuid2"]), lang, returnIds = FALSE);}
   return(sibs);
 }
 
@@ -84,15 +85,15 @@ init <- function(url){
 #' @param rels Data frame of relations between terms.
 #' @param term Give a term ID. 
 #' @param lang Give a language. Default: "la"
-#' @param origin Give an origin. Default: 9000
+#' @param origin Give an origin.
 #' @param returnIds Return IDs or terms in the provided language. Default: FALSE
 #' path()
 #' 
-path <- function(term, lang="la", origin=9000){
+path <- function(term, lang="la", origin="be81554a-7759-11e4-adb6-57ce06b062da"){
   ids <- ancestry(terms, rels, term, lang, origin)
   result <- list()
   for(id in ids){
-    result[[as.character(id)]] <- as.character(terms[terms$term_id == id & terms$lang == lang, "term"])
+    result[[as.character(id)]] <- as.character(terms[terms$uuid == id & terms$lang == lang, "term"])
   }
   result
 }
@@ -105,7 +106,7 @@ path <- function(term, lang="la", origin=9000){
 #' translations()
 #' 
 translations <- function(term){
-    transl <- terms[terms$term_id == term,]
+    transl <- terms[terms$uuid == term,]
     translations <- list()
     for(row in row.names(transl)){
       translations[[as.character(transl[row,"lang"])]] <- as.character(transl[row,"term"])
@@ -129,24 +130,48 @@ smp_api <- function(term){
   api
 }
 
+load_apps <- function(uuid, lang = ""){
+  if(length(row.names(subject_apps[subject_apps$subject == uuid,])) > 0){
+    apps <- subject_apps[subject_apps$subject == uuid,]
+    for(app in apps){
+      app_df <- getURL(as.character(apps[app,"csv_url"]))
+      url <- as.character(apps[app,"root_url"])
+      #param = 
+      #app_list[[apps[app,"name"]]] <<- 
+    }
+  }
+}
+
+add_app <- function(name, icon, subject, type_value, type_level, langs, subject_column, root_url, csv_url){
+  if(type_value != "uuid"){
+    uuid <- get_uuid(subject, value_type)
+  }
+  subject_apps <<- rbind(subject_apps, c(name, icon, uuid, subject, type_value, type_level, langs, subject_column, root_url, csv_url))
+  saveRDS(subject_apps, file="data/subject_apps.rds")
+  subject_list <- tree(uuid, lang, unlist=FALSE)
+  subject_list <- unlist(subject_list)
+  
+                         
+}
+
 #' Ontologies helps you browse your ontologies
 #'
 #' This function allows you to see what ontologies are available.
-#' @param ontoList Data frame of ontologies details. See https://raw.githubusercontent.com/ctzurcanu/smp/master/data/ontologies.csv
+#' @param onto_list Data frame of ontologies.
 #' ontologies()
 #' 
 ontologies <- function(){
-  langs <- levels(ontoList$lang)
+  langs <- levels(onto_list$lang)
   data <- list()
   for(lang in langs){
     data[[lang]] <- list()
-    ids <- as.character(ontoList[ontoList$lang == lang, "subject_id"])
+    ids <- as.character(onto_list[onto_list$lang == lang, "subject_id"])
     for(id in ids){
       data[[lang]][[id]] <- list()
       temp <- list()
-      attr <- names(ontoList)
+      attr <- names(onto_list)
       for(att in attr){
-        temp[[att]] <- as.character(ontoList[ontoList$lang == lang & ontoList$subject_id == id, att])
+        temp[[att]] <- as.character(onto_list[onto_list$lang == lang & onto_list$subject_id == id, att])
       }
       data[[lang]][[id]] <- temp
     }
@@ -159,20 +184,18 @@ ontologies <- function(){
 #' This function allows you browse an ontology by id. Returns a list with keys: id, name, children (for that term).
 #' @param terms Data frame of terms.
 #' @param rels Data frame of relations between terms.
-#' @param term Give a term_id. Defaults to 9000 (Terminologia Morphologica).
+#' @param term Give a uuid.
 #' @param lang Give a display language (la=Latin, en=English, ro=Romanian). Defaults to "la" (Latin).
-#' @param origin Give a term_id for the origin. Defaults to 9000 (Terminologia Morphologica).
+#' @param origin Give a uuid for the origin.
 #' @keywords ontology
 #' @export
 #' @examples
 #' ontobrowse()
 #' 
-ontobrowse <- function(term=9000, lang="la", origin = 9000){
-  term = as.integer(term)
-  origin = as.integer(origin)
+ontobrowse <- function(term="be81554a-7759-11e4-adb6-57ce06b062da", lang="la", origin = ""){
   list <- list()
   list[["id"]] <- term
-  list[["name"]] <- as.character(terms[terms$term_id == term & terms$lang == lang, "term"])[1]
+  list[["name"]] <- as.character(terms[terms$uuid == term & terms$lang == lang, "term"])[1]
   #list[["ancestry"]] <- list()
   list[["haschildren"]] <- 0
   list[["children"]] <- list()
@@ -180,7 +203,7 @@ ontobrowse <- function(term=9000, lang="la", origin = 9000){
 #   parents <- ancestry(terms, rels, term, lang, origin)
 #   if(length(parents) > 0){
 #     for(i in length(parents):2) {
-#       name <- as.character(terms[terms$term_id == parents[i] & terms$lang == lang, "term"])
+#       name <- as.character(terms[terms$uuid == parents[i] & terms$lang == lang, "term"])
 #       temp <- c()
 #       temp["id"] <- parents[i]
 #       temp["name"] <- name
@@ -191,7 +214,7 @@ ontobrowse <- function(term=9000, lang="la", origin = 9000){
   list[["haschildren"]] <- length(kids)
   if(length(kids) > 0) {
     for(i in 1:(length(kids))){
-      name <- as.character(terms[terms$term_id == kids[i] & terms$lang == lang, "term"])
+      name <- as.character(terms[terms$uuid == kids[i] & terms$lang == lang, "term"])
       temp <- list()
       temp[["id"]] <- kids[i]
       temp[["name"]] <- name
@@ -203,7 +226,7 @@ ontobrowse <- function(term=9000, lang="la", origin = 9000){
 #   sibs <- siblings(terms, rels, term, lang)
 #   if(length(sibs) > 1) {
 #     for(i in 1:(length(sibs))){
-#       name <- as.character(terms[terms$term_id == sibs[i] & terms$lang == lang, "term"])
+#       name <- as.character(terms[terms$uuid == sibs[i] & terms$lang == lang, "term"])
 #       temp <- c()
 #       temp["id"] <- sibs[i]
 #       temp["name"] <- name
@@ -218,15 +241,15 @@ ontobrowse <- function(term=9000, lang="la", origin = 9000){
 #' This function allows you browse an ontology from the origin id provided. Returns a multilevel list with keys: id, name, children (recursive, with id, name, children for the subchildren).
 #' @param terms Data frame of terms.
 #' @param rels Data frame of relations between terms.
-#' @param term Give a term_id. Defaults to 9000 (Terminologia Morphologica).
+#' @param term Give a uuid.
 #' @param lang Give a display language (la=Latin, en=English, ro=Romanian). Defaults to "la" (Latin).
-#' @param origin Give a term_id for the origin. Defaults to 9000 (Terminologia Morphologica).
+#' @param origin Give a uuid for the origin.
 #' @keywords ontology
 #' @export
 #' @examples
 #' tree()
 #' 
-tree <- function(term=9000, lang="la", origin = 9000, unlist=TRUE){
+tree <- function(term="be81554a-7759-11e4-adb6-57ce06b062da", lang="la", origin = "", unlist=TRUE){
   list <- ontobrowse(term, lang, origin)
   if(list[["haschildren"]] > 0){
     list <- tree_recursive(term, lang, origin, list)
@@ -252,7 +275,7 @@ tree_recursive <- function(term, lang, origin, list){
 
 # uuid_df <- function(terms){
 #   arr = c()
-#   dup <- duplicated(terms[,"term_id"])
+#   dup <- duplicated(terms[,"uuid"])
 #   indexes <- sort.int(dup, index.return=TRUE)$ix
 #   unique <- indexes[which(dup[indexes] == FALSE)]
 #   for(un in unique){
@@ -265,7 +288,30 @@ tree_recursive <- function(term, lang, origin, list){
 #   terms <- cbind(terms,uuid=arr)
 #   rows <- row.names(terms[terms$uuid == "0",])
 #   for(row in rows){
-#     terms[row,"uuid"]<- as.character(terms[terms$term_id == as.character(terms[row,"term_id"]) & terms$uuid != 0, "uuid"][1])
+#     terms[row,"uuid"]<- as.character(terms[terms$uuid == as.character(terms[row,"uuid"]) & terms$uuid != 0, "uuid"][1])
 #   }
 #   terms
 # }
+
+#' List of ontologies accessed by the RSubject package
+#'
+#' @name onto_list
+#' @docType data
+#' @author Loredana Cirstea \email{loredana.cirstea@gmail.com}
+#' @keywords ontologies
+NULL
+
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+#' @references \url{data_blah.com}
+"diamonds"
